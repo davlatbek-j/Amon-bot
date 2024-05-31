74841066c5e443b7751b97f9b47cf30ecb820bfc
SuperHuma <theferganes@gmail.com>
2024-05-31 17:07:43 +0500

    commit-2

--- src/main/java/uz/amon/domain/entity/Complaint.java
+++ src/main/java/uz/amon/domain/entity/Complaint.java
@@ -1,5 +1,22 @@
 package uz.amon.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
 public class Complaint
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
