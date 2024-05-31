e480aeed168a0ee51b5e99589241ee569a2d44d8
SuperHuma <theferganes@gmail.com>
2024-05-31 16:44:12 +0500

    Initial commit

---
+++ src/main/java/uz/amon/AmonBotApplication.java
@@ -0,0 +1,13 @@
+package uz.amon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmonBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmonBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/amon/AmonBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.amon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmonBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
